7fc716659878696e5f35a8bd8cdeb92ac2a006bc
yonezawanatsuko <nabeta0915@gmail.com>
2025-11-02 10:12:04 +0900

    ６回目のコミット

--- XcodeGit/ContentView.swift
+++ XcodeGit/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
             Text("３回目のコミット")
             Text("４回目のコミット")
             Text("５回目のコミット")
+            Text("６回目のコミット")
         }
         .padding()
     }
